870c14ea6fa65b8e3fc0846dd46057ebb5ab2843
Albert Borsos <albertborsos@me.com>
2014-12-18 10:33:32 +0100

    syntaxHighlighter working version

--- CMSAsset.php
+++ CMSAsset.php
@@ -28,12 +28,21 @@ class CMSAsset extends AssetBundle
             //'css/style.css'.$this->time,
             'css/style.css'.$this->time,
             'vendor/Magnific-Popup/magnific-popup.css'.$this->time,
+			'vendor/syntaxHighlighter/shCoreEclipse.css'.$this->time,
+			'vendor/syntaxHighlighter/shThemeEclipse.css'.$this->time,
         ];
 
         $this->js = [
             //'js/cms.js'.$this->time,
             'js/images.js'.$this->time,
             'vendor/Magnific-Popup/jquery.magnific-popup.min.js'.$this->time,
+            'vendor/syntaxHighlighter/shCore.js'.$this->time,
+            'vendor/syntaxHighlighter/shBrushJScript.js'.$this->time,
+            'vendor/syntaxHighlighter/shBrushPhp.js'.$this->time,
+            'vendor/syntaxHighlighter/shBrushPerl.js'.$this->time,
+            'vendor/syntaxHighlighter/shBrushBash.js'.$this->time,
+            'vendor/syntaxHighlighter/shBrushSql.js'.$this->time,
+            'vendor/syntaxHighlighter/shBrushCss.js'.$this->time,
         ];
     }
 
