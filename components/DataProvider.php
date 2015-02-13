48aed6766f2444008670f070f8f7393c84875616
Albert Borsos <albertborsos@me.com>
2015-02-13 15:38:12 +0100

    S class fix

--- components/DataProvider.php
+++ components/DataProvider.php
@@ -5,6 +5,7 @@ namespace albertborsos\yii2cms\components;
 use albertborsos\yii2cms\models\Languages;
 use albertborsos\yii2cms\models\Posts;
 use albertborsos\yii2cms\models\Users;
+use albertborsos\yii2lib\helpers\S;
 use rmrevin\yii\fontawesome\FA;
 use Yii;
 use yii\helpers\ArrayHelper;
